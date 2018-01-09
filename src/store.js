c69516d31372db163fc68aef88cc869956948428
alberto <github email address>
2018-01-09 17:13:08 -0500

    configured stored

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import players from './reducers/players_reducer'
+
+const reducers = combineReducers({players})
+const middleware = [thunk];
+
+export default createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
