ba9e093856f5088c83bf79057e4acce8824299eb
alberto <github email address>
2018-01-23 21:59:59 -0500

    styled like component

--- src/components/Like.js
+++ src/components/Like.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Like = ({likes, upLike}) => {
     return(
-        <div >
-         <button onClick = {() =>{upLike()}} >Like</button>
+        <div className="likeContainer" >
+         <button className="likeButton" onClick = {() =>{upLike()}} >Like</button>
          <h3>Likes: {likes}</h3>
         </div>
     )
