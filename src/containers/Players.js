4c739adb66a5c6503b6a953f15cf8ef51a9b8260
alberto <github email address>
2018-01-24 18:31:58 -0500

    Moved PlayerCard to containters folder

--- src/containers/Players.js
+++ src/containers/Players.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PlayerCard from '../components/PlayerCard'
+import PlayerCard from '../containers/PlayerCard'
 import { getPlayers } from '../actions/players';
 import { Link } from 'react-router-dom';
 
